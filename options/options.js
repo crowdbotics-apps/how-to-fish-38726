49d50711b60c9986c03bd0f3aa39b6ab48b7e753
Crowdbotics <team@crowdbotics.com>
2023-02-06 00:54:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "how-to-fish-38726",
+  url: "https://how-to-fish-38726.botics.co",
+  api: "https://how-to-fish-38726.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
